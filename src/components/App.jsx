2cc53455975081dcab64d07dbef0232b9382910d
Hi-tei-hi-me <lethargium@gmail.com>
2023-01-16 22:56:09 +0200

    Miscellaneous

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import toast, { Toaster } from 'react-hot-toast';
-import { Box } from './Box/Box.styled';
+import { MdPersonAddDisabled, MdPersonAdd, MdPersonRemove } from 'react-icons/md';
 import { NotFound } from './NotFound/NotFound.styled';
+import { Box } from './Box/Box.styled';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './Contacts/ListOfContacts';
 import { Filter } from './SearchForm/SearchForm';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')) ??
-[]);
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
+  );
   const [filter, applyFilter] = useState('');
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
@@ -20,16 +22,22 @@ export const App = () => {
         return contact.name.trim().toLowerCase() === newContact.name.toLowerCase();
       })
     ) {
-      return toast(`${newContact.name} is already in your Book`, { icon: '❗' });
+      return toast(`${newContact.name} is already in your Book`, {
+        icon: <MdPersonAddDisabled size={25} color="#ccba21" />,
+      });
     }
     const id = nanoid();
     const newAddedContact = { id, ...newContact };
     setContacts(prevState => [newAddedContact, ...prevState]);
-    return toast(`${newContact.name} was successfully added to your Book`, { icon: '✓' });
+    return toast(`${newContact.name} was successfully added to your Book`, {
+      icon: <MdPersonAdd size={28} color="#327047" />,
+    });
   };
   const deleteContact = (id, name) => {
     setContacts(prevState => prevState.filter(contact => contact.id !== id));
-    return toast(`${name} was removed from your Book`, { icon: '✗' });
+    return toast(`${name} was removed from your Book`, {
+      icon: <MdPersonRemove size={25} color="#b24a4a" />,
+    });
   };
   const getFilteredList = () => {
     const normalizedFilter = filter.trim().toLowerCase();
--- src/components/Box/Box.styled.jsx
+++ src/components/Box/Box.styled.jsx
@@ -2,7 +2,10 @@ import styled from '@emotion/styled';
 
 export const Box = styled.div`
   margin: auto;
-  height: 80vh;
+  max-height: 95vh;
   width: 50vw;
   color: #010101;
+  @media (max-width: 767px) {
+    width: 95vw;
+  }
 `;
