6edd2acc399291ab18ed232940437a0572f659ca
Hi-tei-hi-me <lethargium@gmail.com>
2023-01-18 23:45:25 +0200

    Notifications added

---
+++ src/components/Notifications/Notifications.styled.jsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const NothingAdded = styled.div`
+  margin: 15px auto;
+  padding: 20px 40px;
+  border: 1.5px solid #15802c;
+  border-radius: 10px;
+  background-color: #39aa44;
+  text-align: center;
+  vertical-align: middle;
+  color: #aaffac;
+`;
+
+export const NotFound = styled.div`
+  margin: 15px auto;
+  padding: 20px 40px;
+  border: 1.5px solid #801515;
+  border-radius: 10px;
+  background-color: #aa3939;
+  text-align: center;
+  vertical-align: middle;
+  color: #ffaaaa;
+`;
