bea6707de534e0495693c094ca904219f604e9d8
Hi-tei-hi-me <lethargium@gmail.com>
2023-01-18 22:57:30 +0200

    SearchForm remastered

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,16 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { SearchForm, Label, Input } from './SearchForm.styled';
+import { setFilter } from 'redux/searchForm/filtersSlice';
+import { getFilter } from 'redux/searchForm/selectors';
+import { getContacts } from 'redux/contacts/selectors';
 
-export const Filter = ({ value, onChange }) => {
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+  const contacts = useSelector(getContacts);
+  const handleChange = evt => dispatch(setFilter(evt.target.value));
   return (
     <SearchForm>
       <Label htmlFor="filter">Find contact by name:</Label>
       <Input
+        autoFocus
+        disabled={contacts.length < 2 && !filter}
         name="filter"
         type="text"
-        value={value}
-        onChange={evt => onChange(evt.target.value)}
-        autoFocus
+        value={filter}
+        onChange={handleChange}
       />
     </SearchForm>
   );
